fb16ccf2db28a4344c3637fe23a1d5d13b99886f
Mariano Tourné <marianotourne@gmail.com>
2024-09-10 16:37:22 -0300

    updated images url

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Header } from "./components/Header";
 import { ImageGallery } from "./components/ImageGallery";
 import "./index.css";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center">Fermin</h1>
+      <Header />
       <ImageGallery />
     </>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center">Fermin</h1>
+    </>
+  );
+};
