244b1d998910d9995391606768b7a446794c5617
Mariano Tourné <marianotourne@gmail.com>
2024-10-10 15:24:34 -0300

    filter working

--- src/components/FilterPanel.tsx
+++ src/components/FilterPanel.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import { months, Month } from "../data";
 import {
   Label,
@@ -11,21 +10,24 @@ import {
 } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 
-type MonthItemProps = {
-  month: Month;
+type FilterPanelProps = {
+  onMonthChange: (month: number) => void;
+  onYearChange: (year: number) => void;
+  onDescriptionChange: (description: string) => void;
 };
 
-export const FilterPanel: React.FC<MonthItemProps> = () => {
-  const [selected, setSelected] = useState<Month>(months[0]);
-
+export const FilterPanel: React.FC<FilterPanelProps> = ({ onMonthChange }) => {
   return (
-    <Listbox value={selected} onChange={(value: Month) => setSelected(value)}>
+    <Listbox
+      value={months[0]}
+      onChange={(month: Month) => onMonthChange(month.id)}
+    >
       <Label className="block text-sm font-medium leading-6 text-gray-900">
         Mes
       </Label>
       <div className="relative mt-2">
         <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          <span className="block truncate">{selected.name}</span>
+          <span className="block truncate">Mes filtrado</span>
           <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
             <ChevronUpDownIcon
               aria-hidden="true"
